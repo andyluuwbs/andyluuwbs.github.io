17a778c4f15c7778797eac847f8ee8795b33a0c4
Andy Luu <andy.l@wbstech.com.au>
2021-05-06 17:55:14 +1000

    NOISSUE:

--- emiot/flutter_service_worker.js
+++ emiot/flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "60c705f16e1b5a4bc9945b98627fa6ee",
-"index.html": "f8ff23d1deaf42606f2fb708dce6314f",
-"/": "f8ff23d1deaf42606f2fb708dce6314f",
-"main.dart.js": "2bd4b7f0737228b76ba45dd0e4d70d87",
+"index.html": "cddb45678e6403acd18f4ef5bc1b80c6",
+"/": "cddb45678e6403acd18f4ef5bc1b80c6",
+"main.dart.js": "1fb0c980a71ee9f4630c23ae676a2093",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
